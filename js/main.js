249257830988652f42d895a443e1a3df4d534a9e
liam m mahady arch student <lmm28@afsconnect1.njit.edu>
2014-04-23 22:55:39 -0400

    Added the map screen

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+function initialize()
+  {var mapCenter=new google.maps.LatLng(40.741,-74.182);
+  var mapOptions =
+    {center: mapCenter,
+    zoom: 14
+    }
+  var map=new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  }
+google.maps.event.addDomListener(window, 'load', initialize);
