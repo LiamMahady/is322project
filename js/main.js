b01d83c1169ff269817536a3f9902c7c1a53613e
Wesley G Stanulis unknown student <you@example.com>
2014-04-24 15:30:08 -0400

    added map center to your location

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 function initialize()
-  {var mapCenter=new google.maps.LatLng(40.741,-74.182);
+  {var mapCenter=new google.maps.LatLng(60.741,-74.182);
   var mapOptions =
     {center: mapCenter,
     zoom: 14,
     mapTypeControl:false
     }
   var map=new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function (position){
+      inital = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      map.setCenter(inital);
+    });
   }
+ }
 google.maps.event.addDomListener(window, 'load', initialize);
