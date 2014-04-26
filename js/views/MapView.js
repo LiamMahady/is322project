9d0dde5a21b0b8cf47de414d53abeefd18dba424
liam m mahady arch student <lmm28@afsconnect1.njit.edu>
2014-04-26 17:59:53 -0400

    Made the map view store some variables that will be useful

--- js/views/MapView.js
+++ js/views/MapView.js
@@ -4,14 +4,19 @@ var app= app || {};
   app.MapView=Backbone.View.extend
     ({el: '#mapDisplay',
     initialize: function()
-      {var mapCenter=new google.maps.LatLng(60.741,-74.182);
+      {//variables for displaying direction date
+      this.$nextDirectionIcon=this.$('#nextDirectionIcon');
+      this.$directionLeg=this.$('#directionLeg');
+      this.$distanceFooter=this.$('#distanceFooter');
+      var mapCenter=new google.maps.LatLng(60.741,-74.182);
       var mapOptions =
         {center: mapCenter,
         zoom: 14,
         mapTypeControl:false
         }
-      var map=new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+      this.map=new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
       if(navigator.geolocation){
+        var map=this.map;//there must be a better way to get this to getCurrentPosition
         navigator.geolocation.getCurrentPosition(function (position){
           var inital = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
           map.setCenter(inital);
